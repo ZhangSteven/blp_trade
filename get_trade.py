ce495246ae5c6091c3d1c2d28583c64209528919
Steven Zhang <steven.zhang@CLAMC.COM.HK>
2018-10-31 22:10:57 +0800

    test SFTP upload successful

--- get_trade.py
+++ get_trade.py
@@ -3,6 +3,7 @@
 
 from blp_trade.blp import extractTradesToXML
 from blp_trade.utility import get_current_path, get_input_directory
+from utils.sftp import upload
 from os.path import join
 from datetime import datetime
 import logging
@@ -28,15 +29,36 @@ def outputFile():
 
 
 
-def upload(file):
-	pass
+def doUpload(file):
+	upload([file], getUploadConfig())
+
+
+
+def getUploadConfig():
+	"""
+	Returns the upload configuration.
+	"""
+	config = {}
+	config['winscpPath'] = r'C:\Program Files (x86)\WinSCP\WinSCP.com'
+	config['timeout'] = 120
+	config['scriptDir'] = r'C:\Users\steven.zhang\AppData\Local\Programs\Git\git\blp_trade\winscp_scripts'
+	config['logDir'] = r'C:\Users\steven.zhang\AppData\Local\Programs\Git\git\blp_trade\winscp_logs'
+	config['user'] = 'svc_sftp'
+	config['password'] = 'CEcY26' 
+	config['server'] = 'sftp.clamc.com.hk'
+	config['targetDir'] = 'reconciliation'
+	return config
 
 
 
 if __name__ == '__main__':
+	"""
+	Prepare to add a command line argument, so that it does not start upload
+	by default. 
+	"""
 	import logging.config
 	logging.config.fileConfig('logging.config', disable_existing_loggers=False)
 
 	outputFile = outputFile()
 	extractTradesToXML(inputFile(), outputFile)
-	upload(outputFile)
+	doUpload(outputFile)
