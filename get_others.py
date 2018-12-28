c312f256c446ee4308f813e64e1f14347a90c81a
Steven Zhang <steven.zhang@CLAMC.COM.HK>
2018-12-28 09:24:18 +0800

    get_others.py reads local directory file in test mode

--- get_others.py
+++ get_others.py
@@ -31,18 +31,33 @@ def outputFile():
 
 
 if __name__ == '__main__':
+	"""
+	This program works in two modes:
+
+	1. Test mode: the default mode. In this case, it reads an XML file in local
+		directory, run as below:
+
+		$ python get_others.py --file <file name>
+
+	2. Production mode: It reads an XML from a preconfigured directory and the
+		file name is based on the date. The output file will be uploaded to
+		Geneva SFTP server. Run as below:
+
+		$ python get_others.py --mode production
+	"""
 	import logging.config
 	logging.config.fileConfig('logging.config', disable_existing_loggers=False)
 
 	import argparse
 	parser = argparse.ArgumentParser()
+	parser.add_argument('--file', nargs='?', metavar='input file', type=str)
 	parser.add_argument('--mode', nargs='?', metavar='mode', default='test')
 	args = parser.parse_args()
 
 	if args.mode == 'production':
 		inputFile = inputFile()
 	else:
-		inputFile = join(get_current_path(), 'samples', 'TransToGeneva20181031_full.xml')
+		inputFile = join(get_current_path(), args.file)
 
 	try:
 		outputFile = outputFile()
